c1636cf459a30b59aa2787649c9bcfd56b28e2e2
Martin Finke <martin@martin-finke.de>
2020-02-11 10:02:28 +0100

    Apply async drawing even if Component is not opaque

--- modules/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp
+++ modules/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp
@@ -484,14 +484,12 @@ namespace AAXClasses
 #if JUCE_MAC
                     // Create a CALayer for backing content with async drawing. This fixes performance issues on Retina displays with wide color spaces (like Display P3 on the iMac Pro 5K Display). It speeds up drawing on other displays as well.
                     // Taken from https://www.1014.org/index.php?article=802 and https://github.com/aseprite/laf/commit/cf962bffcc9773fa584b2fa7df04ef411a4e17b7
-                    if (component->isOpaque()) {
-                        jassert([(id)nativeViewToAttachTo isKindOfClass:NSView.class]);
-                        NSView* nsView = (NSView*)nativeViewToAttachTo;
-                        nsView.wantsLayer = YES;
-                        jassert(nsView.wantsLayer);
-                        nsView.layer.drawsAsynchronously = YES;
-                        jassert(nsView.layer.drawsAsynchronously);
-                    }
+                    jassert([(id)nativeViewToAttachTo isKindOfClass:NSView.class]);
+                    NSView* nsView = (NSView*)nativeViewToAttachTo;
+                    nsView.wantsLayer = YES;
+                    jassert(nsView.wantsLayer);
+                    nsView.layer.drawsAsynchronously = YES;
+                    jassert(nsView.layer.drawsAsynchronously);
 #endif
                     
                     component->setVisible (true);
