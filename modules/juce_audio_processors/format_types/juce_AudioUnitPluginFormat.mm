d5e9834b76bf816c411405799e585e36a88abbf6
Martin Finke <martin@martin-finke.de>
2022-03-02 18:53:29 +0100

    Fixed compile errors

--- modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
+++ modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
@@ -322,9 +322,9 @@ namespace AudioUnitFormatHelpers
         desc.name = pluginName;
         desc.descriptiveName = pluginName;
         desc.fileOrIdentifier = AudioUnitFormatHelpers::createPluginIdentifier (componentDesc);
-        desc.uid = ((int) componentDesc.componentType)
-                    ^ ((int) componentDesc.componentSubType)
-                    ^ ((int) componentDesc.componentManufacturer);
+        desc.uniqueId = desc.deprecatedUid = ((int) componentDesc.componentType)
+                                           ^ ((int) componentDesc.componentSubType)
+                                           ^ ((int) componentDesc.componentManufacturer);
         desc.lastFileModTime = Time();
         desc.lastInfoUpdateTime = Time::getCurrentTime();
         desc.pluginFormatName = "AudioUnit";
@@ -2671,7 +2671,7 @@ void AudioUnitPluginFormat::findAllTypesForFile (OwnedArray<PluginDescription>& 
 
     auto desc = std::make_unique<PluginDescription>();
     desc->fileOrIdentifier = fileOrIdentifier;
-    desc->uid = 0;
+    desc->uniqueId = desc->deprecatedUid = 0;
     AudioUnitFormatHelpers::fillInPluginDescription(*desc, componentDesc, pluginName, version, manufacturer);
 
     results.add(std::move(desc));
