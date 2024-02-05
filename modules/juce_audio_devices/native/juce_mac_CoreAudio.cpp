91f1018312c2dc9496ec49a87def38b580043f32
Martin Finke <martin@martin-finke.de>
2024-02-05 13:07:58 +0100

    Possible fix for deadlock when switching audio devices

--- modules/juce_audio_devices/native/juce_mac_CoreAudio.cpp
+++ modules/juce_audio_devices/native/juce_mac_CoreAudio.cpp
@@ -738,7 +738,25 @@ public:
                         const AudioBufferList* inInputData,
                         AudioBufferList* outOutputData)
     {
-        const ScopedLock sl (callbackLock);
+        // Use a try-lock to prevent occasional deadlock when switching audio devices.
+        const ScopedTryLock sl (callbackLock, /* acquireLockOnInitialisation: */ true);
+        if (!sl.isLocked()) {
+            constexpr int maxNumAttempts = 20;
+            int attempt = 0;
+            
+            while (true) {
+                Thread::sleep(/* milliseconds: */ 1);
+                if (sl.retryLock())
+                    break;
+                
+                if (attempt++ == maxNumAttempts) {
+                    for (UInt32 i = 0; i < outOutputData->mNumberBuffers; ++i)
+                        zeromem(outOutputData->mBuffers[i].mData, outOutputData->mBuffers[i].mDataByteSize);
+                    
+                    return;
+                }
+            }
+        }
 
         if (audioDeviceStopPending)
         {
