f7bb060bacc37c23854d330014fc7878347c5b35
Martin Finke <martin@martin-finke.de>
2026-01-25 16:33:18 +0700

    Fixed compile error

--- modules/juce_dsp/widgets/juce_WaveShaper.h
+++ modules/juce_dsp/widgets/juce_WaveShaper.h
@@ -73,7 +73,7 @@ struct WaveShaper
 //==============================================================================
 // Although clang supports C++17, their standard library still has no invoke_result
 // support. Remove the "|| JUCE_CLANG" once clang supports this properly!
-#if (! JUCE_CXX17_IS_AVAILABLE) || (JUCE_CLANG && ! JUCE_WINDOWS)
+#if (! JUCE_CXX17_IS_AVAILABLE)
 template <typename Functor>
 static WaveShaper<typename std::result_of<Functor>, Functor> CreateWaveShaper (Functor functionToUse)   { return {functionToUse}; }
 #else
